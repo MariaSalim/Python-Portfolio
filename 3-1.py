585093c75bfe9325a493a70bda4b4a36aa9f606d
Maria Salim <67681138+MariaSalim@users.noreply.github.com>
2025-10-27 04:51:27 +0500

    Update 3-1.py

--- 3-1.py
+++ 3-1.py
@@ -1,12 +1,22 @@
 height = int(input("Enter your height in cm: "))
+bill = 0
 if height >= 120:
     age = input("Enter your age: ")
     age = int(age)
     if age < 12:
-        print("Child tickets are $5")
+        bill = 5
+        print("Child tickets are $5.")
     elif age <= 18:
-        print("Youth tickets are $7")
+        bill = 7
+        print("Youth tickets are $7.")
+    elif age >= 45 and age <= 55:
+        bill = 0
+        print("Your ticket is free!")
     else:
-        print("Adult tickets are $12")
+        bill = 12
+    print(f"Your ticket is ${bill}")
+    if input("Do you want a photo taken? Y or N: ") == 'Y':
+        bill += 3
+        print(f"Your final bill is: ${bill}")
 else:
     print("Sorry, you have to grow taller before you can ride.")
